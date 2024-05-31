0e253a8fdbac118828da790f90d5b71a95fd97da
REDA1000200000 <redabou791@gmail.com>
2024-05-31 14:26:19 +0100

    first commit

---
+++ src/data/experienceData.js
@@ -0,0 +1,17 @@
+const expData = [
+  {
+    id: "2",
+    title:"baccalaureat",
+    date: "  2022 ",
+    description:  "In this role, I study in union school,In the experimental sciences."
+  },
+    {
+      id: "1",
+      title:"Full-stack Developer",
+      date: " october 2022 - Present",
+      description:  "In this role, I developped websites and create frontend pages from ideation to development. I blend creativity with technical expertise to deliver compelling user experiences. My focus spans from initial brainstorming sessions to the final polish of the user interface."
+    }
+    
+];
+
+export default expData;
